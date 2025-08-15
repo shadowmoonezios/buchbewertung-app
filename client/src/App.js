a9052b43ced56562815eda72f8f5e20bfac082d5
shadowmoonezios <agushifelde@hotmail.com>
2025-08-15 14:13:52 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Buchbewertung-App</h1>
+      <p>Willkommen zur Buchbewertung-App!</p>
+    </div>
+  );
+}
+
+export default App;
